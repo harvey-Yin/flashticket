0a88b557aa6f3a8fb182c82b8fa5e50c15259472
harvey yin <1172008090@qq.com>
2026-01-13 11:31:36 +0800

    complete database create and connect

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     
     # 数据库配置：mysql+pymysql://用户名:密码@地址:端口/数据库名
     # 请修改为你自己的 MySQL 信息
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:737602/WXx@localhost:3306/flashticket_db'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:737602@localhost:3306/flashticket_db'
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True # 开发阶段开启，会在控制台打印 SQL 语句，方便学习
